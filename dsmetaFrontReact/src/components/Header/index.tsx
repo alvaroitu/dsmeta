8066200a7d391acc43b17429a2df0b7aa22cf323
Álvaro Itu <contato@alvaroitu.com>
2022-07-16 23:28:19 -0300

    front back and heroku

---
+++ dsmetaFrontReact/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import icon from '../../assets/images/headerIcon.svg'
+
+import './styles.css' 
+
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={icon} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por <a href="https://www.instagram.com/ituoficial">@alvaroitu</a>
+            </p>
+        </div>
+    </header>
+    )
+}
+export default Header
